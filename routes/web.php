0a01878573e8a80f78919f2184fd057fdf84c21f
Sobirov Otabek <devsobirov@gmail.com>
2022-07-07 18:42:46 +0500

    summernote rich-editor & laravel file-manager installed and integrated

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,14 @@ $localizationGroupData = [
     'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
 ];
 
+// Public (Web) routes
 Route::group($localizationGroupData, function() {
     Route::view('/', 'welcome');
 });
 
 Auth::routes();
 
-
+// Admin (Control-panel) routes
 Route::prefix('c-panel')->middleware('auth')->group(function () {
 
     Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -24,4 +25,11 @@ Route::prefix('c-panel')->middleware('auth')->group(function () {
     Route::get('users', [UserController::class, 'index'])->name('users.index');
     Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::view('summernote', 'vendor.summernote.example' )->name('summernote');
+});
+
+// Technical Routes
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
 });
